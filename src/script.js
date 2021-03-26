f80fd0d98efdd61226ff2f95e4dc045c3b5bad17
daria <tochdz@gmail.com>
2021-03-26 12:50:56 +0300

    add cursor animation

--- src/script.js
+++ src/script.js
@@ -39,7 +39,7 @@ fontLoader.load (
         (font) => 
         {
             const textGeometry = new THREE.TextBufferGeometry(
-                'fintech design',
+                'design',
                 {
                     font: font,
                     size: 0.6,
@@ -60,6 +60,7 @@ fontLoader.load (
             const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture})
             material.wireframe = false
             const text = new THREE.Mesh(textGeometry, material)
+            gsap.to(text.rotateZ, {duration:100*(Math.random()*0.5), delay:1})
             scene.add(text)
 
             const octGeometry = new THREE.OctahedronBufferGeometry()
@@ -82,8 +83,8 @@ fontLoader.load (
                 scene.add(oct)
                 var tl = gsap.timeline( {repeat:-1, repeatDelay: 2, yoyo: true} );
                 console.log(tl)
-                gsap.to(oct.position, { duration: 120*(Math.random()*0.5)*Math.PI, delay: 1, x: 20-(Math.random())*Math.PI/Math.random()*0.5-10*Math.random(), y: 10+(Math.random())*Math.PI*0.06 ,z: 23-(Math.random())*Math.PI*0.06})
-                gsap.to(oct.position, { duration: 120*(Math.random()*0.5)*Math.PI, delay: 1, x: 10-(Math.random())*Math.PI/Math.random()*0.5, y: -(Math.random())*Math.PI*0.06, z: 1-(Math.random())*Math.PI*0.06})
+                gsap.to(oct.position, { duration: 120*(Math.random()*0.5)*Math.PI, delay: 1, x: 200-(Math.random())*Math.PI/Math.random()*0.5-10*Math.random(), y: Math.sin(50+(Math.random())*Math.PI*0.06) ,z: 30-(Math.random())*Math.PI*0.06})
+                gsap.to(oct.position, { duration: 120*(Math.random()*0.5)*Math.PI, delay: 1, x: Math.cos(10-(Math.random())*Math.PI/Math.random()*0.5), y: -(Math.random())*Math.PI*0.06, z: -(10-(Math.random())*Math.PI*0.36)})
                 
             }
         }
@@ -93,6 +94,19 @@ fontLoader.load (
  * Object
  */
 
+//cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event)  =>
+{
+   cursor.x = event.clientX /  sizes.width -0.5
+   cursor.y = -(event.clientY /  sizes.height -0.5)
+//    console.log(cursor.x)
+    
+}
+)
 
 /**
  * Sizes
@@ -121,9 +135,9 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(82, sizes.width / sizes.height, 0.2, 100)
+const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.2, 100)
 camera.position.x = 0
-camera.position.y = -0.65
+camera.position.y = -0.7
 camera.position.z = 4
 scene.add(camera)
 console.log(camera)
@@ -153,6 +167,9 @@ const tick = () =>
 
     // octGeometry.rotation.x = -0.15 * elapsedTime
    
+    //update camera
+    camera.position.x = cursor.x * 3
+    camera.position.y = cursor.y * 3
 
     // Update controls
     controls.update()
